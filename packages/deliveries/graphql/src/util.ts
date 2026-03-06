dcd50beef3ec1919c212d3b0af1a1c828ae0e554
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-03-06 18:00:57 +0900

    feat(delivery/graphql): add resolving reference model

--- packages/deliveries/graphql/src/util.ts
+++ packages/deliveries/graphql/src/util.ts
@@ -1,4 +1,4 @@
-import type { FieldType, Manifest } from "@cosmos/core";
+import type { Manifest, Schema } from "@cosmos/core";
 import {
   GraphQLBoolean,
   type GraphQLFieldConfig,
@@ -6,55 +6,92 @@ import {
   GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
-  type GraphQLScalarType,
   GraphQLSchema,
   GraphQLString,
   type ThunkObjMap,
 } from "graphql";
-
-const ScalarMap = {
-  string: GraphQLString,
-  boolean: GraphQLBoolean,
-} satisfies Record<FieldType, GraphQLScalarType>;
 
 export function createSchemaFromManifest(manifest: Manifest): GraphQLSchema {
   const queryFields: ThunkObjMap<
     GraphQLFieldConfig<unknown, unknown>
   > = {};
 
-  for (const collection of manifest.collections) {
-    const fields = collection.meta.reduce<
-      ThunkObjMap<GraphQLFieldConfig<unknown, unknown>>
-    >((acc, cur) => {
-      const base = ScalarMap[cur.type];
-      const type = cur.required ? new GraphQLNonNull(base) : base;
-      const field = {
-        type,
-        description: cur.description || undefined,
-      } satisfies GraphQLFieldConfig<unknown, unknown>;
+  const models = manifest.definitions.map((definition) => {
+    const fields: ThunkObjMap<GraphQLFieldConfig<unknown, unknown>> = () =>
+      definition.schemas.reduce((acc, cur) => {
+        const field = resolveScalarType(cur);
 
-      return {
-        ...acc,
-        [cur.name]: field,
-      };
-    }, {});
+        return {
+          ...acc,
+          [cur.name]: field,
+        };
+      }, {});
 
-    const ModelType = new GraphQLObjectType({
-      name: collection.typeName,
-      fields,
-    });
+    return [
+      new GraphQLObjectType({
+        name: definition.name,
+        fields,
+      }),
+      definition.members,
+    ] satisfies [GraphQLObjectType, string[]];
+  });
 
-    queryFields[collection.name] = {
-      type: ModelType,
-      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-      resolve: (_, { id }) => collection.documents.find((doc) => doc.id === id),
-    };
+  function resolveScalarType(
+    schema: Schema,
+  ): GraphQLFieldConfig<unknown, unknown> {
+    function resolve(): GraphQLFieldConfig<object, unknown> {
+      switch (schema.type) {
+        case "reference": {
+          const model = models.find(([model]) => schema.to === model.name)?.[0];
 
-    queryFields[`all${collection.typeName}s`] = {
-      type: new GraphQLList(ModelType),
-      resolve: () => collection.documents,
+          if (!model) throw new Error("unreachable");
+
+          const field = {
+            type: model,
+            description: schema.description || undefined,
+            resolve: (source) => {
+              const key = Reflect.get(source, model.name);
+
+              return documentMap.get(key);
+            },
+          } satisfies GraphQLFieldConfig<object, unknown>;
+
+          return field;
+        }
+
+        case "boolean": {
+          return { type: GraphQLBoolean };
+        }
+
+        case "string": {
+          return { type: GraphQLString };
+        }
+      }
+    }
+
+    const { type } = resolve();
+    return {
+      type: schema.required ? new GraphQLNonNull(type) : type,
+      description: schema.description || undefined,
     };
   }
+
+  const documentMap = new Map(
+    manifest.entries.map(({ key, value }) => [key, value]),
+  );
+
+  models.forEach(([model, members]) => {
+    queryFields[model.name] = {
+      type: model,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve: (_, { id }) => documentMap.get(id),
+    };
+
+    queryFields[`all${model.name}s`] = {
+      type: new GraphQLList(model),
+      resolve: () => members.map((key) => documentMap.get(key)),
+    };
+  });
 
   return new GraphQLSchema({
     query: new GraphQLObjectType({ name: "Query", fields: queryFields }),
