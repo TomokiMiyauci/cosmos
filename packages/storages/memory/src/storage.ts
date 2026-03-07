4d6f1bb4a6f239a850ea418b854093931640db9c
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-03-07 14:05:47 +0900

    feat(storage/memory): add in-memory storage impl

---
+++ packages/storages/memory/src/storage.ts
@@ -0,0 +1,17 @@
+import type { Storage } from "@cosmos/core";
+
+export class MemoryStorage implements Storage {
+  #map: Map<string, Uint8Array> = new Map();
+
+  read(url: URL): Uint8Array {
+    const content = this.#map.get(url.toString());
+
+    if (!content) throw new Error();
+
+    return content;
+  }
+
+  write(url: URL, conetnt: Uint8Array): void {
+    this.#map.set(url.toString(), conetnt);
+  }
+}
