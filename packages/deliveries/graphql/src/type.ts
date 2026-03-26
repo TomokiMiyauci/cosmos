ad99576170185c008a0c46673f1e418dd265d48d
miyauci <git@miyauchi.dev>
2026-03-26 05:40:08 +0000

    refactor: remove unused

--- packages/deliveries/graphql/src/type.ts
+++ packages/deliveries/graphql/src/type.ts
@@ -1,15 +1,6 @@
 import type { Fetcher } from "@cosmos/core";
 import type { GraphQLFieldConfig, GraphQLObjectType } from "graphql";
 
-export interface QueryFeature {
-  feature: "query";
-  provide: QueryProvider;
-}
-
-export interface QueryProvider {
-  (ctx: QueryContext): GraphQLQueryField[];
-}
-
 export interface QueryContext {
   fetcher: Fetcher;
   entries: GraphEntry[];
