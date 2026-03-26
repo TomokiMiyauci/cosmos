7ddfbf1a4fb740f99534dcaef2fa89e59f3191b2
miyauci <git@miyauchi.dev>
2026-03-26 01:50:10 +0000

    feat(field/datetime): add datetime codec

---
+++ packages/fields/datetime/src/field.ts
@@ -0,0 +1,18 @@
+import type { FieldCodec, Node, StructureValue } from "@cosmos/core";
+
+export class DatetimeFieldCodec implements FieldCodec {
+  parse(structure: StructureValue): Node {
+    if (typeof structure !== "string") throw new SyntaxError();
+
+    return {
+      type: "datetime",
+      value: new Date(structure),
+    };
+  }
+
+  strinigify(node: Node): StructureValue {
+    if (node.type !== "datetime") throw new Error();
+
+    return node.value.toISOString();
+  }
+}
