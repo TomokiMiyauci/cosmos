41e50ca2ee69036f703a2c9f4a8f5a0726b1ff64
miyauci <git@miyauchi.dev>
2026-04-03 05:49:19 +0000

    feat(field/instance): add instance field

---
+++ packages/fields/instance/src/field.ts
@@ -0,0 +1,48 @@
+import type {
+  Field,
+  FieldCodec,
+  FieldContext,
+  Node,
+  StructureValue,
+} from "@cosmos/core";
+
+export class InstanceField implements FieldCodec {
+  async parse(
+    structure: StructureValue,
+    field: Field,
+    ctx: FieldContext,
+  ): Promise<Node> {
+    console.log(structure);
+    if (typeof structure === "string") throw new Error();
+
+    if (field.type !== "map") throw new Error();
+
+    const model = ctx.config.models.find((model) => model.name === field.to);
+
+    if (!model) throw new Error();
+
+    const promises = model.fields.filter((field) => field.name in structure)
+      .map(
+        async (field) => {
+          const key = field.name;
+          const value = structure[key];
+
+          return [
+            key,
+            await ctx.config.field.parse(value, field, ctx),
+          ] as const;
+        },
+      );
+    const entries = await Promise.all(promises);
+    const value = Object.fromEntries(entries);
+
+    return {
+      type: "map",
+      value,
+    };
+  }
+
+  stringify(node: Node): StructureValue | Promise<StructureValue> {
+    throw new Error();
+  }
+}
