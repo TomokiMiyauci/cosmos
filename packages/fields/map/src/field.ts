f8bc67f85223363beaaa162396cbe5327eb138c2
miyauci <git@miyauchi.dev>
2026-04-04 13:11:29 +0000

    feat(field/map): add map field

---
+++ packages/fields/map/src/field.ts
@@ -0,0 +1,39 @@
+import type {
+  Field,
+  FieldCodec,
+  FieldContext,
+  Node,
+  StructureValue,
+} from "@cosmos/core";
+
+export class MapField implements FieldCodec {
+  async parse(
+    structure: StructureValue,
+    field: Field,
+    ctx: FieldContext,
+  ): Promise<Node> {
+    if (typeof structure === "string") throw new SyntaxError();
+    if (field.type !== "map") throw new Error();
+
+    const promises = Object.entries(field.fields).filter(([key]) =>
+      key in structure
+    ).map(async ([key, value]) => {
+      return [
+        key,
+        await ctx.config.field.parse(structure[key], value, ctx),
+      ];
+    });
+
+    const entreis = await Promise.all(promises);
+    const value = Object.fromEntries(entreis);
+
+    return {
+      type: "map",
+      value,
+    };
+  }
+
+  stringify(node: Node): StructureValue | Promise<StructureValue> {
+    throw new Error();
+  }
+}
