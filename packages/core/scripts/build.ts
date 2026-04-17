e3f4c5cb4662d0bcb04c87d352a3119ee87ebae0
miyauci <git@miyauchi.dev>
2026-04-17 01:44:03 +0000

    chore: add build scripts for node

---
+++ packages/core/scripts/build.ts
@@ -0,0 +1,21 @@
+import { build, emptyDir } from "@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/mod.ts"],
+  outDir: "./npm",
+  shims: {},
+  package: {
+    name: "@TomokiMiyauci/cosmos",
+    version: Deno.args[0],
+    publishConfig: {
+      "@TomokiMiyauci:registry": "https://npm.pkg.github.com",
+    },
+    repository: {
+      type: "git",
+      url: "git+https://github.com/TomokiMiyauci/cosmos.git",
+    },
+  },
+  typeCheck: false,
+});
