f6beff41a9f30782a320b382a6e8b858a41af413
miyauci <git@miyauchi.dev>
2026-04-19 02:00:15 +0000

    feat: add markdown parser and serializer

--- packages/core/src/type.ts
+++ packages/core/src/type.ts
@@ -428,12 +428,54 @@ export interface HeadingNodeValue extends Record<string, Node> {
   children: PhrasingContentListNode;
 }
 
-export type PharasingContentMapNode = StrongMapNode | TextMapNode | LinkMapNode;
+export type PharasingContentMapNode =
+  | StrongMapNode
+  | TextMapNode
+  | LinkMapNode
+  | ImageMapNode
+  | BreakMapNode
+  | DeleteMapNode
+  | EmphasisMapNode
+  | HtmlMapNode
+  | ImageReferenceMapNode
+  | InlineCodeMapNode
+  | LinkReferenceMapNode
+  | FootnoteReferenceMapNode;
 
 export type RootNodeMapNode =
   | PharasingContentMapNode
   | HeadingMapNode
-  | ParapraphMapNode;
+  | ParapraphMapNode
+  | BlockquoteMapNode
+  | CodeMapNode
+  | DefinitionMapNode
+  | FootnoteDefinitionMapNode
+  | ListMapNode
+  | ListItemMapNode
+  | TableMapNode
+  | TableCellMapNode
+  | TableRowMapNode
+  | YamlMapNode
+  | ThematicBreakMapNode;
+
+export type BlockContentMapNode =
+  | BlockquoteMapNode
+  | CodeMapNode
+  | HeadingMapNode
+  | HtmlMapNode
+  | ListMapNode
+  | ParapraphMapNode
+  | TableMapNode
+  | ThematicBreakMapNode;
+
+export type ListContentMapNode = ListItemMapNode;
+
+export type TableContentMapNode = TableRowMapNode;
+export type RowContentMapNode = TableCellMapNode;
+
+export type DefinitionContentMapNode =
+  | DefinitionMapNode
+  | FootnoteDefinitionMapNode;
 
 export type RootNodeValue = RootNodeMapNode["value"];
 
@@ -458,6 +500,163 @@ export interface ParapraphMapNode extends MapNode {
   };
 }
 
+export interface BlockquoteMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "blockquote";
+    };
+    children: {
+      type: "list";
+      value: (BlockContentMapNode | DefinitionContentMapNode)[];
+    };
+  };
+}
+
+export interface CodeMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "code";
+    };
+    lang: StringNode;
+    meta: StringNode;
+    value: StringNode;
+  };
+}
+
+export interface DefinitionMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "definition";
+    };
+    identifier: StringNode;
+    label: StringNode;
+    title: StringNode;
+    url: StringNode | AssetNode | ReferenceNode;
+  };
+}
+
+export interface FootnoteDefinitionMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "footnoteDefinition";
+    };
+    identifier: StringNode;
+    label: StringNode;
+    children: {
+      type: "list";
+      value: (BlockContentMapNode | DefinitionContentMapNode)[];
+    };
+  };
+}
+
+export interface FootnoteReferenceMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "footnoteReference";
+    };
+    identifier: StringNode;
+    label: StringNode;
+  };
+}
+
+export interface ListMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "list";
+    };
+    children: {
+      type: "list";
+      value: ListContentMapNode[];
+    };
+    ordered: BooleanNode;
+    spread: BooleanNode;
+    start: NumberNode;
+  };
+}
+
+export interface ListItemMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "listItem";
+    };
+    checked: BooleanNode;
+    children: {
+      type: "list";
+      value: (BlockContentMapNode | DefinitionContentMapNode)[];
+    };
+    spread: BooleanNode;
+  };
+}
+
+export interface TableMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "table";
+    };
+    align: {
+      type: "list";
+      value: {
+        type: "string";
+        value: "center" | "left" | "right";
+      }[];
+    };
+    children: {
+      type: "list";
+      value: TableContentMapNode[];
+    };
+  };
+}
+
+export interface TableCellMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "tableCell";
+    };
+    children: PhrasingContentListNode;
+  };
+}
+
+export interface TableRowMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "tableRow";
+    };
+    children: {
+      type: "list";
+      value: RowContentMapNode[];
+    };
+  };
+}
+
+export interface ThematicBreakMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "thematicBreak";
+    };
+  };
+}
+
+export interface YamlMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "yaml";
+    };
+    value: StringNode;
+  };
+}
+
 export interface LiteralNodeValue extends Record<string, Node> {
   value: StringNode;
 }
@@ -470,5 +669,79 @@ export interface LinkMapNode extends MapNode {
     };
     children: PhrasingContentListNode;
     url: StringNode | AssetNode | ReferenceNode;
+  };
+}
+
+export interface ImageMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "image";
+    };
+    url: StringNode | AssetNode | ReferenceNode;
+    alt: StringNode;
+    title: StringNode;
+  };
+}
+
+export interface BreakMapNode extends MapNode {
+  value: {
+    type: { type: "string"; value: "break" };
+  };
+}
+
+export interface DeleteMapNode extends MapNode {
+  value: {
+    type: { type: "string"; value: "delete" };
+    children: PhrasingContentListNode;
+  };
+}
+
+export interface EmphasisMapNode extends MapNode {
+  value: {
+    type: { type: "string"; value: "emphasis" };
+    children: PhrasingContentListNode;
+  };
+}
+
+export interface HtmlMapNode extends MapNode {
+  value: {
+    type: { type: "string"; value: "html" };
+    value: StringNode;
+  };
+}
+
+export interface ImageReferenceMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "imageReference";
+    };
+    identifier: StringNode;
+    alt: StringNode;
+    label: StringNode;
+    referenceType: { type: "string"; value: ReferenceType };
+  };
+}
+
+export type ReferenceType = "shortcut" | "collapsed" | "full";
+
+export interface InlineCodeMapNode extends MapNode {
+  value: {
+    type: { type: "string"; value: "inlineCode" };
+    value: StringNode;
+  };
+}
+
+export interface LinkReferenceMapNode extends MapNode {
+  value: {
+    type: {
+      type: "string";
+      value: "linkReference";
+    };
+    identifier: StringNode;
+    label: StringNode;
+    referenceType: { type: "string"; value: ReferenceType };
+    children: PhrasingContentListNode;
   };
 }
