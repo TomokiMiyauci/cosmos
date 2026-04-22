971c370707d9583d37e932b774f6e90373b0fcd8
miyauci <git@miyauchi.dev>
2026-04-22 05:55:39 +0000

    test: add test case

--- packages/protocols/graphql/src/type_builder.test.ts
+++ packages/protocols/graphql/src/type_builder.test.ts
@@ -1,6 +1,6 @@
 import test from "./type_builder.test.json" with { type: "json" };
 import { describe, it } from "@std/testing/bdd";
-import { parse, print, printType } from "graphql";
+import { type GraphQLObjectType, parse, print, printType } from "graphql";
 import type { Schema } from "@cosmos/core";
 import { createRoot } from "./type_builder.ts";
 import { expect } from "@std/expect";
@@ -9,17 +9,19 @@ import type { Fetcher } from "./type.ts";
 describe("type_builder", () => {
   for (const testCase of test.cases) {
     it(testCase.description ?? "should match", () => {
+      const map: Record<string, GraphQLObjectType> = {};
       const objectType = createRoot(
         testCase.input.name,
         testCase.input.schema as Schema,
         {
-          map: {},
+          map,
           fetcher: {
             fetch: () => {},
             list: () => {},
           } as unknown as Fetcher,
         },
       );
+      map[testCase.input.name] = objectType;
 
       const type = parse(testCase.output);
       const left = printType(objectType);
