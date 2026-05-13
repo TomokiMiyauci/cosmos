fbef5277e5ded77e1a88ec3ed543e3862803fd5d
miyauci <git@miyauchi.dev>
2026-05-13 02:34:41 +0000

    refactor: restructure GraphqlProtocol constructor and context handling

--- packages/protocols/graphql/src/delivery.ts
+++ packages/protocols/graphql/src/delivery.ts
@@ -1,5 +1,9 @@
 import type { Node, Protocol, ProtocolContext } from "@cosmos/core";
-import { createSchema, createYoga } from "graphql-yoga";
+import {
+  createSchema,
+  createYoga,
+  type YogaServerInstance,
+} from "graphql-yoga";
 import type { GraphQLSchema } from "graphql";
 import type { Fetcher, Plugin, ResolverContext } from "./type.ts";
 
@@ -8,26 +12,30 @@ export interface GraphqlConfig {
 }
 
 export class GraphqlProtocol implements Protocol {
+  #handler: YogaServerInstance<ProtocolContext, ResolverContext>;
   constructor(private schema: GraphQLSchema) {
-  }
-  handle(request: Request, ctx: ProtocolContext): Promise<Response> {
-    const fetcher = {
-      async fetch(id): Promise<Node> {
-        const node = await ctx.datalayer.node.fetch(id);
-
-        return node;
-      },
-      list: ctx.datalayer.node.list.bind(ctx.datalayer.node),
-    } satisfies Fetcher;
-
-    const context = { fetcher } satisfies ResolverContext;
-
-    const yoga = createYoga<Record<PropertyKey, never>, ResolverContext>({
+    const yoga = createYoga<ProtocolContext, ResolverContext>({
       schema: createSchema({ typeDefs: this.schema }),
-      context,
+      context: (ctx) => {
+        const fetcher = {
+          async fetch(id): Promise<Node> {
+            const node = await ctx.datalayer.node.fetch(id);
+
+            return node;
+          },
+          list: ctx.datalayer.node.list.bind(ctx.datalayer.node),
+        } satisfies Fetcher;
+
+        return {
+          fetcher,
+        };
+      },
     });
 
-    const result = yoga(request);
+    this.#handler = yoga;
+  }
+  handle(request: Request, ctx: ProtocolContext): Promise<Response> {
+    const result = this.#handler(request, ctx);
 
     return Promise.resolve(result);
   }
