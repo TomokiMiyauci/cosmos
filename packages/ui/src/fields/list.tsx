46023147bae3b9985f6435966535cc49abcc4720
miyauci <git@miyauchi.dev>
2026-06-19 13:26:38 +0000

    refactor: update ListField component to handle node values and improve rendering logic

--- packages/ui/src/fields/list.tsx
+++ packages/ui/src/fields/list.tsx
@@ -1,31 +1,57 @@
 import type { JSX } from "react";
-import type { Field, ListField, Node } from "@cosmos/core";
+import type { Field, ListField, ListNode, Node } from "@cosmos/core";
 
 export interface ListFieldProps {
   render(
-    props: { field: Field; onChange: OnChange },
+    props: { field: Field; node: Node | null; onChange: OnChange },
   ): JSX.Element;
   onChange: OnChange;
   field: ListField;
+  node: ListNode | null;
 }
 
 interface OnChange {
-  (node: Node): void;
+  (node: Node | null): void;
 }
 
 export default function ListField(props: ListFieldProps): JSX.Element {
-  const { render, onChange, field } = props;
+  const { render, onChange, field, node } = props;
 
-  function onC(node: Node) {
-    onChange({
-      type: "list",
-      value: [node],
-    });
-  }
+  const init = node?.value ?? [];
+  const values = [...init, null] satisfies [...Node[], null];
 
   return (
     <div>
-      {render({ field: field.field, onChange: onC })}
+      <ul>
+        {values.map((child, index) => {
+          function onC(child: Node | null): void {
+            const newValues = child
+              ? init.toSpliced(index, 1, child)
+              : init.toSpliced(index, 1);
+
+            onChange({ type: "list", value: newValues });
+          }
+
+          return (
+            <li key={index}>
+              {render({ field: field.field, node: child, onChange: onC })}
+
+              {child && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    const newValues = init.toSpliced(index, 1);
+
+                    onChange({ type: "list", value: newValues });
+                  }}
+                >
+                  Delete
+                </button>
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
