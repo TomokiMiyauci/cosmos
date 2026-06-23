d8120fbfbe6a897abc5720426feb4c30d8278f0c
miyauci <git@miyauchi.dev>
2026-06-23 11:36:03 +0000

    refactor(service): enhance RestCmsService methods for resource handling and model fetching

--- packages/service/src/mod.ts
+++ packages/service/src/mod.ts
@@ -2,13 +2,14 @@ import { parseToNode } from "@cosmos/parser";
 import type {
   CmsService,
   Content,
+  ContentsOption,
   Entry,
   Field,
   Identity,
   Template,
 } from "@cosmos/ui";
 import { assertContent } from "@cosmos/json";
-import type { Model, Node } from "@cosmos/core";
+import type { Model, Node, Resource } from "@cosmos/core";
 import { mapValues } from "@std/collections/map-values";
 
 export class RestCmsService implements CmsService {
@@ -17,12 +18,31 @@ export class RestCmsService implements CmsService {
     this.#baseUrl = endpoint;
   }
 
-  async findTemplateByFieldId(model: string): Promise<Template | null> {
-    const url = new URL(`./models/${model}`, this.#baseUrl);
+  async findTemplate(resourceId: string): Promise<Template | null> {
+    const baseUrl = this.#baseUrl;
+    const response = await fetch(
+      new URL(`./resources/${resourceId}`, this.#baseUrl),
+    );
+
+    const resource: Resource = await response.json();
+    const modelId = resource.model;
+    const url = new URL(`./models/${modelId}`, this.#baseUrl);
     const modelResponse = await fetch(url);
 
     const result: { model: Model } = await modelResponse.json();
-    const field = modelToField(result.model);
+    const allModels = await findModels(baseUrl);
+
+    const modelRecord = new Map(
+      allModels.map(({ id, model }) => [id, model] as const),
+    );
+
+    const field = modelToField(result.model, (id) => {
+      const value = modelRecord.get(id);
+
+      if (!value) throw new Error();
+
+      return value;
+    });
 
     return {
       field,
@@ -30,8 +50,9 @@ export class RestCmsService implements CmsService {
     };
   }
 
-  async findContentById(id: Content["id"]): Promise<Content> {
+  async findContent(id: Content["id"]): Promise<Content> {
     const url = new URL(`./contents/${id}`, this.#baseUrl);
+    const baseUrl = this.#baseUrl;
 
     const response = await fetch(url);
 
@@ -41,19 +62,23 @@ export class RestCmsService implements CmsService {
       assertContent(json);
 
       const modelId = json.model;
+      const model = await findModel(modelId, baseUrl);
 
-      const url = new URL(`./models/${modelId}`, this.#baseUrl);
+      const allModels = await findModels(baseUrl);
 
-      const modelResponse = await fetch(url);
-
-      if (!response.ok) {
-        return null;
-      }
-
-      const model: { model: Model } = await modelResponse.json();
+      const modelRecord = new Map(
+        allModels.map(({ id, model }) => [id, model] as const),
+      );
 
       const node = parseToNode(json.node);
-      const field = modelToField(model.model);
+
+      const field = modelToField(model, (id) => {
+        const value = modelRecord.get(id);
+
+        if (!value) throw new Error();
+
+        return value;
+      });
 
       return {
         id: json.id,
@@ -63,13 +88,33 @@ export class RestCmsService implements CmsService {
     }
   }
 
-  async queryContents(): Promise<Identity[]> {
+  async findContents(option?: ContentsOption): Promise<Identity[]> {
     const url = new URL(`./contents`, this.#baseUrl);
+
+    const resourceId = option?.resource;
+
+    if (resourceId) {
+      url.searchParams.set("resource", resourceId);
+    }
 
     const response = await fetch(url);
 
     if (response.ok) {
       const json: unknown = await response.json();
+
+      return json;
+    }
+
+    throw new Error();
+  }
+
+  async findResources(): Promise<Identity[]> {
+    const url = new URL("./resources", this.#baseUrl);
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const json = await response.json();
 
       return json;
     }
@@ -93,9 +138,9 @@ export class RestCmsService implements CmsService {
     return response.ok;
   }
 
-  async saveNode(node: Node): Promise<Identity> {
+  async saveNode(resourceId: string, node: Node): Promise<Identity> {
     const url = new URL(`./contents`, this.#baseUrl);
-    const data = { node };
+    const data = { node, resource: resourceId };
     const body = JSON.stringify(data);
     const request = new Request(url, {
       body,
@@ -124,8 +169,37 @@ export class RestCmsService implements CmsService {
   }
 }
 
-function modelToField(model: Model): Field {
-  function to(model: Model, meta?: { required: boolean }): Field {
+async function findModels(baseUrl: URL): Promise<{
+  id: string;
+  model: Model;
+}[]> {
+  const url = new URL(`./models`, baseUrl);
+  const modelResponse = await fetch(url);
+  const result: {
+    id: string;
+    model: Model;
+  }[] = await modelResponse.json();
+
+  return result;
+}
+
+async function findModel(modelId: string, baseUrl: URL): Promise<Model> {
+  const url = new URL(`./models/${modelId}`, baseUrl);
+
+  const modelResponse = await fetch(url);
+  const model: { model: Model } = await modelResponse.json();
+
+  return model.model;
+}
+
+function modelToField(
+  model: Model,
+  getModel: (modelId: string) => Model,
+): Field {
+  function to(
+    model: Model,
+    meta?: { required: boolean },
+  ): Field {
     const required = meta?.required ?? false;
     const description = model.description ?? "";
 
@@ -174,11 +248,20 @@ function modelToField(model: Model): Field {
           field: to(model.field),
         };
       }
-      case "reference":
+      case "reference": {
+        return {
+          type: "reference",
+          candidates: ["a", "b"],
+        };
+      }
+      case "instance": {
+        const childModel = getModel(model.model);
+
+        return to(childModel);
+      }
       case "asset":
       case "union":
-      case "markdown":
-      case "instance": {
+      case "markdown": {
         throw new Error();
       }
     }
