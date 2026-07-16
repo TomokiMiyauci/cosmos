e8abc2b07858521bd4e853da37a26264f2848efe
miyauci <git@miyauchi.dev>
2026-07-16 01:58:24 +0000

    feat(api): export rest client

---
+++ packages/api/src/client.ts
@@ -0,0 +1,9 @@
+import { initClient, type InitClientReturn } from "@ts-rest/core";
+import { contract } from "./contract.ts";
+
+export function createClient(baseUrl: URL): Client {
+  return initClient(contract, { baseUrl: baseUrl.href });
+}
+
+export interface Client
+  extends InitClientReturn<typeof contract, { baseUrl: string }> {}
--- packages/api/src/mod.ts
+++ packages/api/src/mod.ts
@@ -1,4 +1,4 @@
-export { RestCmsService } from "./services/ui.ts";
 export { createRestHandler } from "./handler.ts";
 export { type QueryOptions, type Reader } from "./application/query.ts";
 export { type EntryDto, type SummaryDTO } from "./application/dto.ts";
+export { type Client, createClient } from "./client.ts";
