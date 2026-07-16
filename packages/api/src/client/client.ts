c0eaf673b57ba0e54d0d93f6ddcf509f61dce3ec
miyauci <git@miyauchi.dev>
2026-07-16 02:09:40 +0000

    refactor(api): move client as sub module

--- packages/api/src/client/client.ts
+++ packages/api/src/client/client.ts
@@ -1,5 +1,5 @@
 import { initClient, type InitClientReturn } from "@ts-rest/core";
-import { contract } from "./contract.ts";
+import { contract } from "../contract.ts";
 
 export function createClient(baseUrl: URL): Client {
   return initClient(contract, { baseUrl: baseUrl.href });
--- packages/api/src/mod.ts
+++ packages/api/src/mod.ts
@@ -1,4 +1,3 @@
 export { createRestHandler } from "./handler.ts";
 export { type QueryOptions, type Reader } from "./application/query.ts";
 export { type EntryDto, type SummaryDTO } from "./application/dto.ts";
-export { type Client, createClient } from "./client.ts";
