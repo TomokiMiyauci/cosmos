53a590bcd06183dd847789b23421cd70f933865d
miyauci <git@miyauchi.dev>
2026-07-21 14:22:53 +0000

    chore: add example client entrypoint

--- examples/studio/client.tsx
+++ examples/studio/client.tsx
@@ -1,18 +1,21 @@
 /// <reference lib="dom" />
 
-import { Admin, Router } from "@cosmos/ui";
+import { Admin, en, I18n, Router } from "@cosmos/ui";
+import { RestCmsService } from "@cosmos/ui/rest";
 import { hydrateRoot } from "react-dom/client";
-import { Service } from "@cosmos/service";
 import { API_ENDPOINT } from "./constant.ts";
 
 const url = new URL(location.href);
-const router = new Router();
-const result = router.route(url);
+const service = new RestCmsService(new URL(API_ENDPOINT));
+const router = new Router(service);
+const i18n = new I18n(en);
+const result = await router.route(url);
 
 hydrateRoot(
   globalThis.document,
   <Admin
     route={result}
-    service={new Service(new URL(API_ENDPOINT))}
+    service={service}
+    translation={i18n}
   />,
 );
