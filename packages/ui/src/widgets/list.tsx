def597b587123ce4725312a52e1014710614d7de
miyauci <git@miyauchi.dev>
2026-08-29 02:44:08 +0000

    feat(ui): implement ListControl component and extend WidgetProps interface

---
+++ packages/ui/src/widgets/list.tsx
@@ -0,0 +1,40 @@
+import type { JSX } from "react";
+import type { WidgetProps } from "./type.ts";
+
+export default function ListControl(props: WidgetProps): JSX.Element {
+  const { definition, render, api } = props;
+
+  if (definition.type !== "list") throw new Error();
+
+  const list = api.useList();
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          list.append();
+        }}
+      >
+        Add
+      </button>
+
+      {[...list].map((id, index) => {
+        return (
+          <div key={id}>
+            {render(definition.item, index.toString())}
+
+            <button
+              type="button"
+              onClick={() => {
+                list.remove(id);
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- packages/ui/src/widgets/type.ts
+++ packages/ui/src/widgets/type.ts
@@ -1,4 +1,59 @@
+import type { ReactNode } from "react";
+import type { Schema } from "../application/query.ts";
+
 export interface WidgetProps {
   value: string | null;
   onChange(value: string | null): void;
+  render(def: Definition, group?: string): ReactNode;
+  definition: Definition;
+  api: Api;
+}
+
+interface Api {
+  useList(): List;
+}
+
+export interface List {
+  [Symbol.iterator](): IterableIterator<Id>;
+  append(): void;
+  remove(id: Id): void;
+}
+
+type Id = string;
+
+export interface Widget {
+  (props: WidgetProps): ReactNode;
+}
+
+export type Definition =
+  | StringDefinition
+  | NumberDefinition
+  | BooleanDefinition
+  | ListDefinition
+  | MapDefinition
+  | UnionDefinition;
+
+interface ListDefinition extends Schema.List, BaseDefinition {
+  item: Definition;
+}
+
+interface BaseDefinition {
+  presentation: Presentation;
+}
+
+interface StringDefinition extends Schema.String, BaseDefinition {}
+
+interface NumberDefinition extends Schema.Number, BaseDefinition {}
+
+interface BooleanDefinition extends Schema.Boolean, BaseDefinition {}
+
+interface MapDefinition extends Schema.Map, BaseDefinition {
+  props: Record<string, { field: Definition }>;
+}
+
+interface UnionDefinition extends Schema.Union, BaseDefinition {}
+
+export interface Presentation {
+  title: string;
+  widget: Widget;
 }
