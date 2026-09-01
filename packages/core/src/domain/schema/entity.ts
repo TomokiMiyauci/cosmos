c3ca52530fbc960088177a19b258246a87025453
miyauci <git@miyauchi.dev>
2026-09-01 13:27:07 +0000

    feat(core): change schema entity interface to contain schema definition

--- packages/core/src/domain/schema/entity.ts
+++ packages/core/src/domain/schema/entity.ts
@@ -1,68 +1,27 @@
 import { SchemaId } from "./id.ts";
 import type { SchemaRepository } from "./repository.ts";
+import type { Schema as SchemaDefinition } from "@cosmos/schema";
 
-export type Schema = PrimitiveSchema | CompositeSchema | ReferenceSchema;
+export class Schema {
+  #id: SchemaId;
+  #definition: SchemaDefinition;
 
-export interface BaseSchema {
-  id: SchemaId;
-}
+  private constructor(id: SchemaId, definition: SchemaDefinition) {
+    this.#id = id;
+    this.#definition = definition;
+  }
 
-export type PrimitiveSchema =
-  | LiteralSchema
-  | StringSchema
-  | NumberSchema
-  | BooleanSchema
-  | TemporalSchema;
+  static of(id: SchemaId, definition: SchemaDefinition): Schema {
+    return new Schema(id, definition);
+  }
 
-export type CompositeSchema = MapSchema | ListSchema | UnionSchema;
+  get id(): SchemaId {
+    return this.#id;
+  }
 
-export interface LiteralSchema extends BaseSchema {
-  type: "literal";
-  value: string;
-}
-
-export interface StringSchema extends BaseSchema {
-  readonly type: "string";
-  readonly format: StringFormat | null;
-}
-
-export type StringFormat = "url" | "email";
-
-export interface NumberSchema extends BaseSchema {
-  type: "number";
-}
-
-export interface BooleanSchema extends BaseSchema {
-  type: "boolean";
-}
-
-export interface MapSchema extends BaseSchema {
-  type: "map";
-  properties: Record<string, MapProperty>;
-}
-
-export interface MapProperty {
-  required: boolean;
-  schema: Schema;
-}
-
-export interface ListSchema extends BaseSchema {
-  type: "list";
-  schema: Schema;
-}
-
-export interface ReferenceSchema extends BaseSchema {
-  type: "reference";
-  schema: Schema;
-}
-
-export interface UnionSchema extends BaseSchema {
-  type: "union";
-  members: Schema[];
-}
-
-export interface TemporalSchema extends BaseSchema {
-  type: "temporal";
+  get definition(): SchemaDefinition {
+    return this.#definition;
+  }
 }
 
 // deno-lint-ignore no-namespace
@@ -71,4 +30,5 @@ export namespace Schema {
   export type Id = SchemaId;
 
   export type Repository = SchemaRepository;
+  export type Definition = SchemaDefinition;
 }
