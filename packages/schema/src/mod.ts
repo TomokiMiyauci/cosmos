0eb0ba4dd987b72f945e011707ad7d8b0a2dcd34
miyauci <git@miyauchi.dev>
2026-09-01 12:04:39 +0000

    feat(schema): add schema definition

---
+++ packages/schema/src/mod.ts
@@ -0,0 +1,11 @@
+export type {
+  BooleanSchema,
+  MapSchema,
+  NumberSchema,
+  ReferenceSchema,
+  Schema,
+  SequenceSchema,
+  StringSchema,
+  TemporalSchema,
+  UnionSchema,
+} from "./schema.ts";
---
+++ packages/schema/src/schema.ts
@@ -0,0 +1,46 @@
+export type Schema =
+  | StringSchema
+  | NumberSchema
+  | BooleanSchema
+  | TemporalSchema
+  | MapSchema
+  | SequenceSchema
+  | ReferenceSchema
+  | UnionSchema;
+
+export interface StringSchema {
+  type: "string";
+}
+
+export interface NumberSchema {
+  type: "number";
+}
+
+export interface BooleanSchema {
+  type: "boolean";
+}
+
+export interface MapSchema {
+  type: "map";
+  properties: Record<string, Schema>;
+  required: string[];
+}
+
+export interface SequenceSchema {
+  type: "sequence";
+  item: Schema;
+}
+
+export interface TemporalSchema {
+  type: "temporal";
+}
+
+export interface ReferenceSchema {
+  type: "reference";
+  target: Schema;
+}
+
+export interface UnionSchema {
+  type: "union";
+  members: Schema[];
+}
