b17e4d45ef62de7d4ee1e9dd944eb8bf536f9a40
miyauci <git@miyauchi.dev>
2026-09-02 07:07:32 +0000

    feat(schema-field): export related types

--- packages/schema_field/src/mod.ts
+++ packages/schema_field/src/mod.ts
@@ -1,8 +1,10 @@
 export {
+  type FieldError,
   Fields,
   type FieldsProps,
   type Path,
   useFields,
+  type UseFieldsReturn,
   type Value,
 } from "./field.tsx";
-export type { Widget, WidgetProps } from "./type.ts";
+export type { Definition, Widget, WidgetProps } from "./type.ts";
