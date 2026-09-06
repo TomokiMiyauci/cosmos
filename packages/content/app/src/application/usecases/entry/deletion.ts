9a22ce470c2763dd374a1b319f5bade7e9bb479b
miyauci <git@miyauchi.dev>
2026-09-06 01:13:41 +0000

    feat(content): improve error handling in EntryDeleteUseCase

--- packages/content/app/src/application/usecases/entry/deletion.ts
+++ packages/content/app/src/application/usecases/entry/deletion.ts
@@ -1,16 +1,22 @@
 import { Entry } from "@cosmos/core";
 import { Result } from "@miyauci/util";
 
+export type DeletionError = InvalidIdError;
+
+export interface InvalidIdError {
+  type: "INVALID_ID";
+}
+
 export class EntryDeleteUseCase {
   constructor(private repositry: Entry.Repositry) {}
 
-  async execute(id: string): Promise<Result<void, Error>> {
+  async execute(id: string): Promise<Result<void, DeletionError>> {
     const [entryId, error] = Entry.Id.of(id);
 
-    if (error) return Result.error(new Error());
+    if (error) return Result.error({ type: "INVALID_ID" });
 
     await this.repositry.delete(entryId);
 
-    return Result.ok(undefined);
+    return Result.ok(void 0);
   }
 }
