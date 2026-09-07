6bc75578540a063170e9d8bef3ee67676f7fd7b4
miyauci <git@miyauchi.dev>
2026-09-07 12:30:44 +0000

    chore(schema-field): change import declareation name

--- packages/schema_field/src/fields/boolean.tsx
+++ packages/schema_field/src/fields/boolean.tsx
@@ -1,17 +1,19 @@
 import type { JSX } from "react";
-import type { FieldProps, Primitive, PrimitiveFieldValue } from "./type.ts";
+import type { FieldProps } from "./type.ts";
 import { createUseList } from "./util.ts";
 import { useController } from "react-hook-form";
+import type { FieldValue } from "../type.ts";
 
 export default function BooleanField(props: FieldProps): JSX.Element {
-  const { name, definition: def, render, layout: Layout } = props;
+  const { name, definition: def, render, layout: Layout, required = true } =
+    props;
   const useList = createUseList(name);
   const { fieldState: { error } } = useController({ name });
 
   const api = {
     useList,
     useValue(): [string | null, (value: string | null) => void] {
-      const { field } = useController<PrimitiveFieldValue>({ name });
+      const { field } = useController<FieldValue>({ name });
       const value = field.value ?? null;
 
       if (!(value === null || typeof value === "boolean")) {
@@ -22,8 +24,10 @@ export default function BooleanField(props: FieldProps): JSX.Element {
         ? value.toString()
         : value;
 
-      return [v, (value: Primitive | null) => {
-        const v = typeof value === "string" ? Boolean(value) : null;
+      return [v, (value: string | null) => {
+        const v = typeof value === "string"
+          ? value === "true" ? true : value === "false" ? false : null
+          : null;
 
         field.onChange(v);
       }];
@@ -38,7 +42,7 @@ export default function BooleanField(props: FieldProps): JSX.Element {
           definition={def}
           render={(def) => render({ name, definition: def })}
           api={api}
-          required
+          required={required}
         />
       }
       error={error?.message ?? null}
