ad28b6eaafdb72de74290585cf3a1f24442ff02e
miyauci <git@miyauchi.dev>
2026-09-15 05:01:40 +0000

    feat: export controls as submodule

---
+++ packages/ui/src/control.ts
@@ -0,0 +1,15 @@
+import TextControl from "./controls/text.tsx";
+import MapControl from "./controls/kv.tsx";
+import NumericControl from "./controls/numeric.tsx";
+import CheckboxControl from "./controls/checkbox.tsx";
+import ListControl from "./controls/list.tsx";
+import DateControl from "./controls/date.tsx";
+
+export {
+  CheckboxControl,
+  DateControl,
+  ListControl,
+  MapControl,
+  NumericControl,
+  TextControl,
+};
--- packages/ui/src/mod.ts
+++ packages/ui/src/mod.ts
@@ -1,22 +1,8 @@
 export { Admin, type AdminProps } from "./admin.tsx";
 export { Page, Router } from "./router.ts";
 export {
-  type CmsService,
-  type Content,
-  type ContentsOption,
-  type Entry,
-  type Field,
-  type Identity,
-  type Summary,
-  type Template,
-} from "./type.ts";
-export { I18n } from "./translation.ts";
-export { type Queries } from "./application/query.ts";
+  type DefinitionQuery,
+  type EntryQuery,
+  type Queries,
+} from "./application/query.ts";
 export { type Services } from "./application/service.ts";
-import en from "./locales/en.json" with { type: "json" };
-
-import TextControl from "./widgets/text.tsx";
-
-export { TextControl };
-
-export { en };
